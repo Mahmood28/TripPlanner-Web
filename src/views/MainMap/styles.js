5a4657c0d3ff535dde98bb672296ec096d4fd6e2
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-28 17:55:42 +0300

    styling

--- src/views/MainMap/styles.js
+++ src/views/MainMap/styles.js
@@ -1,4 +1,13 @@
 import styled from "styled-components";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Divider,
+  CardHeader,
+} from "@material-ui/core";
 
 export const StyledMapContainer = styled.div`
   position: relative;
@@ -8,4 +17,53 @@ export const StyledMapContainer = styled.div`
 
 export const StyledMapElement = styled.div`
   height: 100%;
+`;
+
+export const InfoCard = styled(Card)`
+  && {
+    border-radius: 12px;
+    text-align: center;
+  }
+`;
+
+export const StyledHeader = styled(CardHeader)`
+  && {
+    text-align: center;
+    spacing: 10;
+    font-size: 16px;
+  }
+`;
+
+export const ButtonContainer = styled(CardActions)`
+  && {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+export const StyledButton = styled(Button)`
+  && {
+    padding: 5px;
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+`;
+
+export const StyledContainer = styled.div`
+  display: flex;
+`;
+
+export const StarContainer = styled(StyledContainer)`
+  justify-content: center;
+  flex: 1;
+  margin-bottom: 10px;
+`;
+
+export const DialogContainer = styled(StyledContainer)`
+  justify-content: space-between;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
 `;
