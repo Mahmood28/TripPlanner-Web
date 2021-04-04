3ba6a03f8dc2d239cac1f96202babed2e6029bda
JannatJameel <j_annat@outlook.com>
2021-04-04 12:51:57 +0300

    minor change

--- src/views/ActivityDetail/index.js
+++ src/views/ActivityDetail/index.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 // Components
 import ActivityCard from "views/ActivityDetail/ActivityCard";
 import ReviewList from "views/ActivityDetail/ReviewList";
-import EditReview from "views/Profile/EditReview";
 
 const ActivityDetail = () => {
   const { activities } = useSelector((state) => state.activityReducer);
@@ -19,7 +18,6 @@ const ActivityDetail = () => {
     <>
       <ActivityCard activity={foundActivity} />
       <ReviewList activity={foundActivity} />
-      <EditReview />
     </>
   );
 };
